84bcdc0f886ec2a24dda86d33160f4f534d1722f
CodeWithAsmit <mohanasmit@gmail.com>
2025-03-28 23:36:52 +0530

    Added handling for dotenv in file so that it gets loaded before app starts

--- src/main/java/com/asmit/JobApp/JobApplication.java
+++ src/main/java/com/asmit/JobApp/JobApplication.java
@@ -3,13 +3,17 @@ package com.asmit.JobApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class JobApplication {
 
-	public static void main(String[] args) {
+public class JobApplication
+{
+	public static void main(String[] args)
+	{
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(JobApplication.class, args);
 	}
-
 }
